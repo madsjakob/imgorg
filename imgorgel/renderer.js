21b65f814889a301f9308b02ad77b251c291e9f6
Mads Jakob Steffansen <mads.steffansen@cb.dk>
2018-11-20 10:07:26 +0100

    Cycle through images

--- imgorgel/renderer.js
+++ imgorgel/renderer.js
@@ -6,32 +6,103 @@ const path = require('path');
 
 let displayImages = function displayImages(filepaths, bookmarks) {
     console.log(filepaths);
+    let img = document.getElementById('currentImg');
     let dirPath = filepaths[0];
     let mappeNavn = document.getElementById('mappe');
     mappeNavn.value = dirPath;
-    let target = document.getElementById('image_container');
-    target.innerHTML = "";
+    img.imgIndex = -1;
     fs.readdir(dirPath, (err, dir) => {
         console.log(dir);
         if(dir.length > 0) {
-            let ext = path.extname(dir[0]).toLocaleLowerCase();                
-            if (ext == '.png') {
-                imgType = 'data:image/png;base64';
+            let imgArr = [];
+            for(let index = 0; index < dir.length; index++) {
+                imgArr.push(path.join(dirPath, dir[index]));
             }
-            else if (ext == '.jpg') {
-                imgType = 'data:image/jpg;base64';
-            }
-            if(imgType != null) {
-                let filename = path.join(dirPath, dir[0]);
-                let imgData = fs.readFileSync(filename).toString('base64');
-                let imgTag = '<div class="card"><img class="card-img-top" src="' + imgType + ',' + imgData + '" alt="Card image cap">'
-                + ' <div class="card-body"><p class="card-text">' + dir[0] + '</p>'
-                + '<p class="card-text">' + dir.length + ' items</p>'
-                + '</div></div>';
-                target.insertAdjacentHTML('beforeend', imgTag);
-            }
+            img.imgArr = imgArr;
+            img.imgIndex = 0;
+            displayImage();
         }
     });
+}
+
+let displayImage  = function displayImage() {
+    let img = document.getElementById('currentImg');
+    let imgRow = document.getElementById('imgRow');
+    let btnRow = document.getElementById('btnRow');
+    if(img.imgIndex >= 0 && img.imgArr && img.imgArr.length > 0) {
+        imgRow.style.visibility = "visible";
+        btnRow.style.visibility = "visible";
+        let filepath = img.imgArr[img.imgIndex];
+        let ext = path.extname(filepath).toLocaleLowerCase();                
+        if (ext == '.png') {
+            imgType = 'data:image/png;base64';
+        }
+        else if (ext == '.jpg') {
+            imgType = 'data:image/jpg;base64';
+        }
+        else if (ext == '.gif') {
+            imgType = 'data:image/gif;base64';
+        }
+        
+        let imgData = fs.readFileSync(filepath).toString('base64');                
+        img.src = imgType + "," + imgData;
+    } else {
+        btnRow.style.visibility = "hidden"
+        imgRow.style.visibility = "hidden"
+    }
+}
+
+let nextImage = function nextImage() {
+    let img = document.getElementById('currentImg');
+    if(img.imgIndex >= 0 && img.imgArr && img.imgArr.length > 0) { 
+        img.imgIndex = img.imgIndex + 1;
+        if(img.imgIndex >= img.imgArr.length) {
+            img.imgIndex = 0;
+        }
+        displayImage();
+    }    
+}
+
+let removeImage = function removeImage() {
+    let img = document.getElementById('currentImg');
+    if(img.imgIndex >= 0 && img.imgArr && img.imgArr.length > 0) { 
+        img.imgArr.splice(img.imgIndex, 1);
+        if (img.imgArr.length > 0)
+        {
+            if(img.imgIndex >= img.imgArr.length) {
+                img.imgIndex = 0;
+            }
+        } else {
+            img.imgIndex = -1;
+        }
+        displayImage();
+    }    
+}
+
+let getFilename = function getFilename() {
+    let result = null;
+    let img = document.getElementById('currentImg');
+    if(img.imgIndex >= 0 && img.imgArr && img.imgArr.length > 0) { 
+        result =  img.imgArr[img.imgIndex];
+    }
+    return result;
+}
+
+let archiveImage = function archiveImage() {
+    let filename = getFilename();
+    if (filename) {
+        console.log("Arkiverer " + filename);
+        removeImage();
+    }
+}
+
+let discardImage = function discardImage() {
+    let filename = getFilename();
+    if (filename) {
+        console.log("Kasserer " + filename);
+        removeImage();
+    }
+
 }
 
 let selectDirectory = function selectDirectory(){
@@ -47,48 +118,22 @@ filebutton.addEventListener('click', _ => {
     selectDirectory();
 });
 
+let nextbutton = document.getElementById('btnnext');
+nextbutton.addEventListener('click', _ => {
+    console.log('click');
+    nextImage();
+});
+
+let discardbutton = document.getElementById('btndiscard');
+discardbutton.addEventListener('click', _ => {
+    discardImage();
+});
+let archivebutton = document.getElementById('btnarchive');
+archivebutton.addEventListener('click', _ => {
+    archiveImage();
+});
+
+
 console.log(process);
 console.log(process.type);
-let mem = process.getSystemMemoryInfo();
-let output = `
-            <h2 class="page-header">App Version Data</h2>
-            <ul class="list-group">
-                <li class="list-group-item">Platform: ${process.platform}</li>
-                <li class="list-group-item">Process ID: ${process.pid}</li>
-                <li class="list-group-item">Node: ${process.versions.node}</li>
-                <li class="list-group-item">Chrome: ${process.versions.chrome}</li>
-                <li class="list-group-item">Electron: ${process.versions.electron}</li>
-            </ul>
-            <h2 class="page-header">System specs</h2>
-            <ul class="list-group">
-                <li class="list-group-item">System Architechture: ${process.arch}</li>
-                <li class="list-group-item">Processor Identifier: ${process.env.PROCESSOR_IDENTIFIER}</li>
-                <li class="list-group-item">Printer: ${process.env.PRINTER}</li>
-            </ul>
-            <h2 class="page-header">System Memory</h2>
-            <ul class="list-group">
-                <li class="list-group-item">Total: ${mem.total}</li>
-                <li class="list-group-item">Free: ${mem.free}</li>
-                <li class="list-group-item">Swap Total: ${mem.swapTotal}</li>
-                <li class="list-group-item">Swap Free: ${mem.swapFree}</li>
-            </ul>
-    `;
-    // document.getElementById('output').innerHTML = output;
-// remote.dialog.showOpenDialog(remote.getCurrentWindow(),
-// {
-//     filters: [
-//         { name: 'Images', extensions: ['png'] }
-//     ]
-// },
-// function (filepaths, bookmarks) {
-//     //read image (note: use async in production)
-//     var _img = fs.readFileSync(filepaths[0]).toString('base64');
-//     //example for .png
-//     var _out = '<img src="data:image/png;base64,' + _img + '" />';
-//     //render/display
-//     var _target = document.getElementById('image_container');
-//     _target.insertAdjacentHTML('beforeend', _out);
-
-//     return;
-// });
 
